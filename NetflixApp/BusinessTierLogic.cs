acc77f0fd6d46982e494b0b068c7943f7797314e
Adam Socik <socikadam@gmail.com>
2015-01-10 20:59:35 -0600

    Added project

---
+++ NetflixApp/BusinessTierLogic.cs
@@ -0,0 +1,361 @@
+﻿//
+// Netflix Database Application using N-Tier Design. 
+//
+// Adam Socik
+//
+
+//
+// BusinessTier:  business logic, acting as interface between UI and data store.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+
+namespace BusinessTier
+{
+    //
+    // Business
+    //
+    public class Business
+    {
+        //
+        // Fields
+        //
+        private string _DBFile;
+        private DataAccessTier.Data datatier;
+
+        //
+        // Constructor
+        //
+        public Business(string DatabaseFilename)
+        {
+            _DBFile = DatabaseFilename;
+            datatier = new DataAccessTier.Data(DatabaseFilename);
+        }
+
+        //
+        // TestConnection
+        //
+        // Returns true if we can establish a connection to the database, false if not.
+        //
+        public bool TestConnection()
+        {
+            return datatier.TestConnection();
+        }
+
+        //
+        // GetMovie
+        //
+        // Retrieves Movie object based on MOVIE ID; returns null if movie is not
+        // found.
+        //
+        public Movie GetMovie(int MovieID)
+        {
+            string sql = string.Format("SELECT MovieName FROM Movies WHERE MovieID={0};", MovieID);
+            object result = datatier.ExecuteScalarQuery(sql);
+
+            if (result == null || result.ToString() == "")
+                return null;
+            return new Movie(MovieID, result.ToString());
+        }
+
+        //
+        // GetMovie
+        //
+        // Retrieves Movie object based on MOVIE NAME; returns null if movie is not
+        // found.
+        //
+        public Movie GetMovie(string MovieName)
+        {
+            string sql = string.Format("SELECT MovieID FROM Movies WHERE MovieName='{0}';", MovieName);
+            object result = datatier.ExecuteScalarQuery(sql);
+
+            if (result == null || result.ToString() == "")
+                return null;
+            return new Movie(System.Convert.ToInt32(result.ToString()), MovieName);
+        }
+
+        //
+        // AddMovie
+        //
+        // Adds the movie, returning a Movie object containing the name and the 
+        // movie's id.  If the add failed, null is returned.
+        //
+        public Movie AddMovie(string MovieName)
+        {
+            string sql = string.Format(@"
+                INSERT INTO Movies(MovieName) Values('{0}');
+                SELECT MovieID FROM Movies WHERE MovieID = SCOPE_IDENTITY();", MovieName);
+            object result = datatier.ExecuteScalarQuery(sql);
+
+            if (result == null)
+                return null;
+
+            return new Movie(System.Convert.ToInt32(result.ToString()), MovieName);
+        }
+
+        //
+        // AddReview
+        //
+        // Adds review based on MOVIE ID, returning a Review object containing
+        // the review, review's id, etc.  If the add failed, null is returned.
+        //
+        public Review AddReview(int MovieID, int UserID, int Rating)
+        {
+            string sql = string.Format(@"
+                INSERT INTO Reviews(MovieID, UserID, Rating) Values({0}, {1}, {2});
+                SELECT ReviewID FROM Reviews WHERE ReviewID = SCOPE_IDENTITY();",
+                MovieID, UserID, Rating);
+            object result = datatier.ExecuteScalarQuery(sql);
+
+            if (result == null)
+                return null;
+
+            return new Review(System.Convert.ToInt32(result.ToString()), MovieID, UserID, Rating);
+        }
+
+        //
+        // GetMovieDetail
+        //
+        // Given a MOVIE ID, returns detailed information about this movie --- all
+        // the reviews, the total number of reviews, average rating, etc.  If the 
+        // movie cannot be found, null is returned.
+        //
+        public MovieDetail GetMovieDetail(int MovieID)
+        {
+            Movie movie = GetMovie(MovieID);
+            if (movie == null)
+                return null;
+
+            // Find the average rating of the movie
+            string sql = string.Format(@"
+                SELECT ROUND(AVG(CAST(Rating AS Float)), 4) AS AvgRating 
+                FROM Reviews
+				INNER JOIN Movies ON Reviews.MovieID = Movies.MovieID
+				WHERE Movies.MovieName='{0}';", (movie.MovieName).Replace("'", "''"));
+            object result = datatier.ExecuteScalarQuery(sql);
+            double avgRating = 0;
+            if (result != null)
+                avgRating = System.Convert.ToDouble(result.ToString());
+
+            // Get all of the reviews for the movie
+            int numReviews = 0;
+            List<Review> reviews = new List<Review>();
+
+            sql = string.Format(@"
+                SELECT ReviewID, UserID, Rating 
+                FROM Reviews 
+                WHERE MovieID={0}
+                ORDER BY Rating Desc, UserID ASC;", MovieID);
+            DataSet ds = datatier.ExecuteNonScalarQuery(sql);
+
+            // If the data set was empty then return with empty results (movie, 0, 0, empty list)
+            if (ds == null)
+                return new MovieDetail(movie, avgRating, numReviews, reviews.AsReadOnly());
+
+            DataTable dt = ds.Tables["TABLE"];
+
+            // Create the reviews list
+            foreach (DataRow row in dt.Rows)
+            {
+                int reviewid = System.Convert.ToInt32((row["ReviewID"]).ToString());
+                int userid = System.Convert.ToInt32((row["UserID"]).ToString());
+                int rating = System.Convert.ToInt32((row["Rating"]).ToString());
+                reviews.Add(new Review(reviewid, MovieID, userid, rating));
+                numReviews++;
+            }
+
+            return new MovieDetail(movie, avgRating, numReviews, reviews.AsReadOnly());
+        }
+
+        //
+        // GetUserDetail
+        //
+        // Given a USER ID, returns detailed information about this user --- all
+        // the reviews submitted by this user, the total number of reviews, average 
+        // rating given, etc.  If the user cannot be found, null is returned.
+        //
+        public UserDetail GetUserDetail(int UserID)
+        {
+            User user = new User(UserID);
+            double avgRating = 0;
+            int numReviews = 0;
+            List<Review> reviews = new List<Review>();
+
+            // Find the count of ratings
+            string sql = string.Format(@"SELECT COUNT(Rating) FROM Reviews WHERE UserID={0};", UserID);
+            object result = datatier.ExecuteScalarQuery(sql);
+
+            // User could not be found
+            if (result == null)
+                return null;
+
+            numReviews = System.Convert.ToInt32(result.ToString());
+
+            // Get the average rating for the user
+            sql = string.Format(@"SELECT AVG(Rating) FROM Reviews WHERE UserID={0}", UserID);
+            result = datatier.ExecuteScalarQuery(sql);
+            avgRating = System.Convert.ToInt32(result.ToString());
+
+            sql = string.Format(@"SELECT ReviewID, MovieID, Rating FROM Reviews WHERE UserID={0}", UserID);
+            DataSet ds = datatier.ExecuteNonScalarQuery(sql);
+
+            // If the data set was empty then return with empty results 
+            if (ds == null)
+                return new UserDetail(user, avgRating, numReviews, reviews.AsReadOnly());
+
+            // Create the reviews list
+            DataTable dt = ds.Tables["TABLE"];
+            foreach (DataRow row in dt.Rows)
+            {
+                int reviewid = System.Convert.ToInt32((row["ReviewID"]).ToString());
+                int movieid = System.Convert.ToInt32((row["MovieID"]).ToString());
+                int rating = System.Convert.ToInt32((row["Rating"]).ToString());
+                reviews.Add(new Review(reviewid, movieid, UserID, rating));
+            }
+
+            return new UserDetail(user, avgRating, numReviews, reviews.AsReadOnly());
+        }
+
+        //
+        // GetTopMoviesByAvgRating
+        //
+        // Returns the top N movies in descending order by average rating.  If two
+        // movies have the same rating, the movies are presented in ascending order
+        // by name.  If N < 1, an EMPTY LIST is returned.
+        //
+        public IReadOnlyList<Movie> GetTopMoviesByAvgRating(int N)
+        {
+            List<Movie> movies = new List<Movie>();
+            string sql = string.Format(@"
+                SELECT TOP {0} Movies.MovieName, Movies.MovieID 
+                FROM Movies
+                INNER JOIN 
+                (
+                    SELECT MovieID, ROUND(AVG(CAST(Rating AS Float)), 4) as AvgRating 
+                    FROM Reviews
+                    GROUP BY MovieID
+                ) g
+                ON g.MovieID = Movies.MovieID
+                ORDER BY g.AvgRating DESC, Movies.MovieName Asc;", N);
+            DataSet ds = datatier.ExecuteNonScalarQuery(sql);
+
+            // If the data set was empty then return with empty results 
+            if (ds == null)
+                return movies;
+
+            // Create the reviews list
+            DataTable dt = ds.Tables["TABLE"];
+            foreach (DataRow row in dt.Rows)
+            {
+                string movieName = (row["MovieName"]).ToString();
+                int movieID = System.Convert.ToInt32((row["MovieID"]).ToString());
+                movies.Add(new Movie(movieID, movieName));
+            }
+
+            return movies;
+        }
+
+        //
+        // GetTopMoviesByNumReviews
+        //
+        // Returns the top N movies in descending order by number of reviews.  If two
+        // movies have the same number of reviews, the movies are presented in ascending
+        // order by name.  If N < 1, an EMPTY LIST is returned.
+        //
+        public IReadOnlyList<Movie> GetTopMoviesByNumReviews(int N)
+        {
+            List<Movie> movies = new List<Movie>();
+            string sql = string.Format(@"
+                SELECT TOP {0} Movies.MovieName, Movies.MovieID 
+                FROM Movies
+                INNER JOIN
+                (
+                    SELECT MovieID, COUNT(*) as RatingCount 
+                    FROM Reviews
+                    GROUP BY MovieID
+                ) g
+                ON g.MovieID = Movies.MovieID
+                ORDER BY g.RatingCount DESC, Movies.MovieName Asc;", N);
+
+            DataSet ds = datatier.ExecuteNonScalarQuery(sql);
+
+            // If the data set was empty then return with empty results 
+            if (ds == null)
+                return movies;
+
+            // Create the reviews list
+            DataTable dt = ds.Tables["TABLE"];
+            foreach (DataRow row in dt.Rows)
+            {
+                string movieName = (row["MovieName"]).ToString();
+                int movieID = System.Convert.ToInt32((row["MovieID"]).ToString());
+                movies.Add(new Movie(movieID, movieName));
+            }
+            
+            return movies;
+        }
+
+        //
+        // GetTopUsersByNumReviews
+        //
+        // Returns the top N users in descending order by number of reviews.  If two
+        // users have the same number of reviews, the users are presented in ascending
+        // order by user id.  If N < 1, an EMPTY LIST is returned.
+        //
+        public IReadOnlyList<User> GetTopUsersByNumReviews(int N)
+        {
+            List<User> users = new List<User>();
+            string sql = string.Format(@"
+                SELECT TOP {0} UserID, COUNT(*) AS RatingCount
+                FROM Reviews
+                GROUP BY UserID
+                ORDER BY RatingCount DESC, UserID Asc;", N);
+            DataSet ds = datatier.ExecuteNonScalarQuery(sql);
+
+            // If the data set was empty then return with empty results 
+            if (ds == null)
+                return users;
+
+            // Create the reviews list
+            DataTable dt = ds.Tables["TABLE"];
+            foreach (DataRow row in dt.Rows)
+            {
+                int userid = System.Convert.ToInt32((row["UserID"]).ToString());
+                users.Add(new User(userid));
+            }
+      
+            return users;
+        }
+
+        //
+        // GetAllMovies
+        //
+        // Returns a list of all movies in the database in alphabetical order
+        //
+        public IReadOnlyList<Movie> GetAllMovies()
+        {
+            List<Movie> movies = new List<Movie>();
+            string sql = string.Format(@"SELECT * FROM Movies");
+            DataSet ds = datatier.ExecuteNonScalarQuery(sql);
+
+            // If the data set was empty then return with empty results 
+            if (ds == null)
+                return null;
+
+            // Create the reviews list
+            DataTable dt = ds.Tables["TABLE"];
+            foreach (DataRow row in dt.Rows)
+            {
+                string movieName = (row["MovieName"]).ToString();
+                int movieID = System.Convert.ToInt32((row["MovieID"]).ToString());
+                movies.Add(new Movie(movieID, movieName));
+            }
+
+            return movies;
+        }
+
+    }//class
+}//namespace
---
+++ NetflixApp/Form1.cs
@@ -0,0 +1,310 @@
+﻿//
+// Netflix Database Application using N-Tier Design. 
+//
+// Adam Socik
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetflixApp
+{
+	public partial class Form1 : Form
+	{
+		//
+		// Class members:
+		//
+		private Random RandomNumberGenerator;
+
+		//
+		// Constructor:
+		//
+		public Form1()
+		{
+			InitializeComponent();
+			RandomNumberGenerator = new Random();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        //
+        // Test Connection
+        //
+        private void cmdConnect_Click(object sender, EventArgs e)
+        {
+            BusinessTier.Business bt = new BusinessTier.Business(this.txtFileName.Text);
+            if (bt.TestConnection() == true)
+                MessageBox.Show("Connected");
+            else
+                MessageBox.Show("Not connected");
+        }
+
+        //
+        // Get Movie Name:  from id
+        //
+		private void cmdGetMovieName_Click(object sender, EventArgs e)
+		{
+            listBox1.Items.Clear();
+            int id = System.Convert.ToInt32(this.txtMovieID.Text);
+
+            BusinessTier.Business bt = new BusinessTier.Business("netflix.mdf");
+            BusinessTier.Movie movie = bt.GetMovie(id);
+
+            if (movie == null)
+                listBox1.Items.Add("Movie not found...");
+            else
+                listBox1.Items.Add(movie.MovieName);
+        }
+
+        //
+        // Get Movie Reviews
+        //
+		private void cmdGetMovieReviews_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int id = System.Convert.ToInt32(this.txtMovieID.Text);
+
+            BusinessTier.Business bt = new BusinessTier.Business("netflix.mdf");
+            BusinessTier.Movie movie = bt.GetMovie(id);
+
+            if (movie == null)
+                listBox1.Items.Add("Movie not found...");
+            else
+            {
+                var detail = bt.GetMovieDetail(movie.MovieID);
+                foreach (var review in detail.Reviews)
+                    listBox1.Items.Add(review.UserID + ": " + review.Rating);
+            }
+        }
+
+		//
+		// Average Rating
+		//
+		private void cmdAvgRating_Click(object sender, EventArgs e)
+		{
+            listBox1.Items.Clear();
+            string name = txtRatingsMovieName.Text;
+            name = name.Replace("'", "''");
+
+            BusinessTier.Business bt = new BusinessTier.Business("netflix.mdf");
+            BusinessTier.Movie movie = bt.GetMovie(name);
+
+            if (movie == null)
+                listBox1.Items.Add("Movie not found...");
+            else
+            {
+                var detail = bt.GetMovieDetail(movie.MovieID);
+                listBox1.Items.Add("Average rating: " + detail.AvgRating);
+            }
+		}
+
+		//
+		// Each Rating
+		//
+		private void cmdEachRating_Click(object sender, EventArgs e)
+		{
+            listBox1.Items.Clear();
+            string name = txtRatingsMovieName.Text;
+            name = name.Replace("'", "''");
+
+            BusinessTier.Business bt = new BusinessTier.Business("netflix.mdf");
+            BusinessTier.Movie movie = bt.GetMovie(name);
+
+            if (movie == null)
+                listBox1.Items.Add("Movie not found...");
+            else
+            {
+                var detail = bt.GetMovieDetail(movie.MovieID);
+                int ones = 0;
+                int twos = 0;
+                int threes = 0;
+                int fours = 0;
+                int fives = 0;
+
+                foreach(var review in detail.Reviews)
+                {
+                    switch (review.Rating)
+                    {
+                        case 1:
+                            ones++;
+                            break;
+                        case 2:
+                            twos++;
+                            break;
+                        case 3:
+                            threes++;
+                            break;
+                        case 4:
+                            fours++;
+                            break;
+                        case 5:
+                            fives++;
+                            break;
+                    }
+                }
+                listBox1.Items.Add("5: " + fives);
+                listBox1.Items.Add("4: " + fours);
+                listBox1.Items.Add("3: " + threes);
+                listBox1.Items.Add("2: " + twos);
+                listBox1.Items.Add("1: " + ones);
+                listBox1.Items.Add("Total: " + (ones + twos + threes + fours + fives));
+            }
+		}
+	
+		//
+		// Add movie
+		//
+		private void cmdInsertMovie_Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Clear();
+            string name = txtInsertMovieName.Text;
+            name = name.Replace("'", "''"); 
+
+            BusinessTier.Business bt = new BusinessTier.Business("netflix.mdf");
+            BusinessTier.Movie movie = bt.AddMovie(name);
+
+            if (movie != null)
+                listBox1.Items.Add("Success, movie added with id: " + movie.MovieID);
+            else
+                listBox1.Items.Add("** Insert failed?! **");
+		}
+
+		private void tbarRating_Scroll(object sender, EventArgs e)
+		{
+			lblRating.Text = tbarRating.Value.ToString();
+		}
+
+		//
+		// Add Review
+		//
+		private void cmdInsertReview_Click(object sender, EventArgs e)
+		{
+            listBox1.Items.Clear();
+            string name = txtInsertMovieName.Text;
+            name = name.Replace("'", "''");
+
+            BusinessTier.Business bt = new BusinessTier.Business("netflix.mdf");
+            BusinessTier.Movie movie = bt.GetMovie(name);
+
+            if (movie == null)
+                listBox1.Items.Add("Movie not found...");
+            else
+            {
+                int userid = RandomNumberGenerator.Next(100000, 999999);  // 6-digit user ids:
+                BusinessTier.Review review = bt.AddReview(movie.MovieID, userid, System.Convert.ToInt32(lblRating.Text));
+
+                if (review != null)
+                    listBox1.Items.Add("Success, review added at id: " + review.ReviewID);
+                else
+                    listBox1.Items.Add("** Insert failed?! **");
+            }
+	    }
+
+        //
+        // Top N Movies by Average Rating
+        //
+        private void cmdTopMoviesByAvgRating_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string N = txtTopN.Text; 
+
+            BusinessTier.Business bt = new BusinessTier.Business("netflix.mdf");
+            IReadOnlyList<BusinessTier.Movie> movies = bt.GetTopMoviesByAvgRating(Int32.Parse(N));
+
+            if (movies.Count() == 0)
+            {
+                listBox1.Items.Add("**Error, or database is empty?!");
+            }
+            else
+            {
+                foreach (var movie in movies)
+                {
+                    var detail = bt.GetMovieDetail(movie.MovieID);
+                    listBox1.Items.Add(detail.AvgRating + "\t" + movie.MovieName);
+                }
+            }
+        }
+
+        //
+        // Top N Movies by # of reviews
+        //
+        private void cmdTopMoviesByNumReviews_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string N = txtTopN.Text;
+
+            BusinessTier.Business bt = new BusinessTier.Business("netflix.mdf");
+            IReadOnlyList<BusinessTier.Movie> movies = bt.GetTopMoviesByNumReviews(Int32.Parse(N));
+
+            if (movies.Count() == 0)
+            {
+                listBox1.Items.Add("**Error, or database is empty?!");
+            }
+            else
+            {
+                foreach (var movie in movies)
+                {
+                    var detail = bt.GetMovieDetail(movie.MovieID);
+                    listBox1.Items.Add(detail.NumReviews + "\t" + movie.MovieName);
+                }
+            }
+        }
+
+        //
+        // Top N Users by # of reviews
+        //
+        private void cmdTopUsers_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string N = txtTopN.Text;
+
+            BusinessTier.Business bt = new BusinessTier.Business("netflix.mdf");
+            IReadOnlyList<BusinessTier.User> users = bt.GetTopUsersByNumReviews(Int32.Parse(N));
+
+            if (users.Count() == 0)
+            {
+                listBox1.Items.Add("**Error, or database is empty?!");
+            }
+            else
+            {
+                foreach (var user in users)
+                {
+                    var detail = bt.GetUserDetail(user.UserID);
+                    listBox1.Items.Add(detail.NumReviews + "\t" + user.UserID);
+                }
+            }
+        }
+
+        private void printMovies_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            BusinessTier.Business bt = new BusinessTier.Business("netflix.mdf");
+            IReadOnlyList<BusinessTier.Movie> movies = bt.GetAllMovies();
+
+
+            if (movies == null)
+                listBox1.Items.Add("Movies not found...");
+            else
+            {
+                foreach(var movie in movies)
+                    listBox1.Items.Add(movie.MovieID + "\t" + movie.MovieName);
+            }
+        }
+
+        private void txtMovieID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+	}//class
+}//namespace
